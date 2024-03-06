614507488687b79bf5a5de4c8294f581fa189039
Emmanuel González Contreras <143232185+EmmGC@users.noreply.github.com>
2024-03-06 09:14:53 -0600

    Version 1.5
    ya sirve todo y bien

---
+++ Ecuaciones/Termino.java
@@ -0,0 +1,16 @@
+package Ecuaciones;
+public class Termino 
+{
+    private double coeficiente;
+    private int exponente;
+
+    public Termino(double coeficiente, int exponente) 
+    {
+        this.coeficiente = coeficiente;
+        this.exponente = exponente;
+    }
+
+    public double evalua(double x) {
+        return coeficiente * Math.pow(x, exponente);
+    }
+}
